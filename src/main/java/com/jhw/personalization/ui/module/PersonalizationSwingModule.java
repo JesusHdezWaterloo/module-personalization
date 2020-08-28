3a438e6ce1a5db8e014edb92f7ef86d0a7e9211d
Unknown <unknown@example.com>
2020-08-27 20:07:41 -0500

    SwingModule: extends default

--- src/main/java/com/jhw/personalization/ui/module/PersonalizationSwingModule.java
+++ src/main/java/com/jhw/personalization/ui/module/PersonalizationSwingModule.java
@@ -1,15 +1,12 @@
 package com.jhw.personalization.ui.module;
 
 import com.clean.core.app.services.ExceptionHandler;
-import com.clean.swing.app.AbstractSwingApplication;
-import com.clean.swing.app.AbstractSwingMainModule;
+import com.clean.swing.app.DefaultAbstractSwingMainModule;
 import com.jhw.personalization.services.PersonalizationHandler;
 import com.jhw.personalization.services.PersonalizationResourceService;
 import java.net.MalformedURLException;
 
-public class PersonalizationSwingModule implements AbstractSwingMainModule {
-
-    private final PersonalizationModuleNavigator navigator = new PersonalizationModuleNavigator();
+public class PersonalizationSwingModule extends DefaultAbstractSwingMainModule {
 
     private PersonalizationSwingModule() {
     }
@@ -25,16 +22,4 @@ public class PersonalizationSwingModule implements AbstractSwingMainModule {
         return new PersonalizationSwingModule();
     }
 
-    @Override
-    public void register(AbstractSwingApplication app) {
-    }
-
-    @Override
-    public void navigateTo(String string, Object... o) {
-        navigator.navigateTo(string, o);
-    }
-
-    @Override
-    public void closeModule() {
-    }
 }
