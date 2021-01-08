b08a1cc31a0313b8e80d162f09ff4719bcbd2270
Unknown <unknown@example.com>
2021-01-08 18:21:47 -0500

    Licence a clases

--- src/main/java/com/root101/module/util/personalization/repo/repo_impl/PersonalizationRepoImpl.java
+++ src/main/java/com/root101/module/util/personalization/repo/repo_impl/PersonalizationRepoImpl.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 Root101 (jhernandezb96@gmail.com, +53-5-426-8660).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Or read it directly from LICENCE.txt file at the root of this project.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.root101.module.util.personalization.repo.repo_impl;
 
 import com.root101.clean.core.app.services.NotificationHandler;
@@ -17,10 +33,9 @@ import com.root101.json.jackson.JACKSON;
 import java.awt.Color;
 
 /**
- * Implementacion de la Interfaz {@code LicenceRepo} para manejar el
- * comportamiento de las definiciones de su interfaz
  *
- * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
+ * @author Root101 (jhernandezb96@gmail.com, +53-5-426-8660)
+ * @author JesusHdezWaterloo@Github
  */
 public class PersonalizationRepoImpl extends JACKSONRepoGeneral<Personalization> implements PersonalizationRepo {
 
